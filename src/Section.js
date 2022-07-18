410355a5502087fa47c6833738b320f51b2d7420
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-07-18 11:05:14 +0100

    Updated css
    Added image as background
    Fixed buttons css
    Fixed Sub header layout

--- src/Section.js
+++ src/Section.js
@@ -2,14 +2,11 @@ import React from "react";
 import "./Section.css";
 
 export default function  Section() {
+
+
     return (
         <div className="Section">
-            <img src="https://tesla-cdn.thron.com/delivery/public/image/tesla/8e2df1b9-a4bf-4eb9-beec-2cf5cc77fca0/bvlatuR/std/0x0/8e2df1b9-a4bf-4eb9-beec-2cf5cc77fca0" alt="tesla car" />
-            <img src="https://tesla-cdn.thron.com/delivery/public/image/tesla/7fe1c903-6f21-4fa6-b2df-13fe5b4b45d3/bvlatuR/std/4096x2560/M3-Homepage-Desktop-RHD" alt="tesla car" />
-            <img src="https://tesla-cdn.thron.com/delivery/public/image/tesla/2391415a-7273-4735-92b2-a37c84c26f04/bvlatuR/std/4096x2560/Homepage-Model-S-Desktop-RHD" alt="tesla car" />
-            <img src="https://tesla-cdn.thron.com/delivery/public/image/tesla/27d0055c-b0bf-476c-b3aa-ec59d314f871/bvlatuR/std/0x0/27d0055c-b0bf-476c-b3aa-ec59d314f871" alt="tesla car" />
-            <img src="https://tesla-cdn.thron.com/delivery/public/image/tesla/8e2b1be8-91e2-441c-bf0f-4a1490fd22f0/bvlatuR/std/4096x2560/Homepage-Model-X-Desktop-RHD" alt="tesla car" />
-            <img src="https://tesla-cdn.thron.com/delivery/public/image/tesla/dd739764-bcaa-4263-9488-8c73bc9fb046/bvlatuR/std/0x0/dd739764-bcaa-4263-9488-8c73bc9fb046" alt="tesla car" />
+            <div className="image" style={{ backgroundImage: `url("https://tesla-cdn.thron.com/delivery/public/image/tesla/8e2df1b9-a4bf-4eb9-beec-2cf5cc77fca0/bvlatuR/std/0x0/8e2df1b9-a4bf-4eb9-beec-2cf5cc77fca0")`}}></div>
         </div>
     );
 }
--- src/SubHeader.js
+++ src/SubHeader.js
@@ -1,16 +1,17 @@
 import React from "react";
+import "./SubHeader.css";
 
 export default function  SubHeader() {
     return (
       <div className="SubHeader">
       <div>
-        <div className="title">Model Y</div>
-        <div className="subtitle">2% Company Car Tax. Learn More</div>
+        <h1 className="title">Model Y</h1>
+        <p className="subtitle">2% Company Car Tax. <a href="./pages.learnMore.html" alt="Learn More">Learn More</a></p>
       </div>
       <div className="sub-header">
          <button className="custom-order">CUSTOM ORDER</button>
          <button className="test-drive">TEST DRIVE</button>
-         <div className="test-drive">Schedule a Test Drive</div>
+         <div className="test-drive-link">Schedule a Test Drive</div>
       </div>
       </div>
     );
